fa7f65cc721eae51446fb3292d63873a9415937c
futureots <futureots113@gmail.com>
2026-05-12 18:21:53 +0900

    feat : ResultPanel 기능 추가

---
+++ Assets/02.Script/05.Network/NetworkInGame/ResultPanel.cs
@@ -0,0 +1,48 @@
+
+using Fusion;
+using TMPro;
+using UnityEngine;
+
+public class ResultPanel : NetworkBehaviour
+{
+    public TextMeshPro resultText;
+
+    bool isShowed = false;
+    
+    [Rpc(RpcSources.All,RpcTargets.All)]
+    void RPC_ShowResultPanel()
+    {
+        // 여러번 실행되지 않도록 제한(굳이 필요 없긴함)
+        if (isShowed) return;
+        isShowed = true;
+        
+        // 값을 DataManager에서 가져옴.
+        var dataManager = DataManager.Instance;
+        var text = "Null";
+        if (dataManager)
+        {
+            var time = dataManager.timer.GetTime();
+            var anchorCount = dataManager.AnchorCount;
+            var fallCount =  dataManager.FallCount;
+            text = $"Time : {Timer.ConvertTimeToString(time)} \nAnchor : {anchorCount} \nFall : {fallCount}";
+        }
+        else Debug.LogWarning("DataManager가 존재하지 않습니다.");
+        
+        // text 패널을 활성화
+        resultText.gameObject.SetActive(true);
+        
+        // Text에 해당 값을 규격에 맞게 작성
+        resultText.text = text;
+    }
+
+    /// <summary>
+    /// 정상에 도달해 게임을 종료할 때 사용하는 함수
+    /// </summary>
+    public void OnInteract()
+    {
+        // 게임 종료 함수 실행
+        GameManager.Instance?.GameEnd();
+        // 지금까지 누적된 데이터들을 가져와서 표시
+        RPC_ShowResultPanel();
+    } 
+}
--- Assets/02.Script/05.Network/NetworkInGame/Timer.cs
+++ Assets/02.Script/05.Network/NetworkInGame/Timer.cs
@@ -28,6 +28,13 @@ public class Timer : NetworkBehaviour
         return _lastDisplayedSecond;
     }
 
+    public static string ConvertTimeToString(int time)
+    {
+        var min =  time / 60;
+        var sec =  time % 60;
+        return $"{min}:{sec}";
+    }
+
     /// <summary> 현재 걸린 시간 </summary>
     [Networked] private float ElapsedTime { get; set; }
 
